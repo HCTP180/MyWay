ec94bfde7e9d876282ecc166eba78cd428161545
emrekosen <emrekosen1@gmail.com>
2018-12-02 01:55:30 +0300

    findGasStations added.

--- nearbyPlaces.py
+++ nearbyPlaces.py
@@ -1,7 +1,21 @@
 import googlemaps
 
-gmaps = googlemaps.Client(key='AIzaSyDjSIZfI_fviDx3h-Wo1U9qZsK8fhHrXzA')
 
-gmaps2 = gmaps.places_nearby(location=(41.0612191,28.6884175), keyword="Benzin İstasyonu", language="tr", name="Benzin İstasyonu",open_now=True, rank_by="distance", type="gas_station")
+def findGasStations():
+    gmaps = googlemaps.Client(key='AIzaSyDjSIZfI_fviDx3h-Wo1U9qZsK8fhHrXzA')
 
-print(gmaps2)
+    gmaps2 = gmaps.places_nearby(location=(41.0612191, 28.6884175), keyword="Benzin İstasyonu", language="tr",
+                                 name="Benzin İstasyonu", open_now=True, rank_by="distance", type="gas_station")
+
+    nearby_gas_stations = dict()
+
+    for i in gmaps2['results']:
+        name = i['name']
+        geometry = i['geometry']
+        location = geometry['location']
+        lat = location['lat']
+        lng = location['lng']
+        nearby_gas_stations[name] = (lat, lng)
+
+    return nearby_gas_stations
+
