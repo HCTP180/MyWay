4195c10aadb5fe2ce065d74f94ae3f3721482f50
emrekosen <emrekosen1@gmail.com>
2018-12-02 02:22:46 +0300

    findRestaurants function added.

--- nearbyPlaces.py
+++ nearbyPlaces.py
@@ -19,3 +19,21 @@ def findGasStations():
 
     return nearby_gas_stations
 
+
+def findRestaurants():
+    gmaps = googlemaps.Client(key='AIzaSyDjSIZfI_fviDx3h-Wo1U9qZsK8fhHrXzA')
+
+    gmaps2 = gmaps.places_nearby(location=(41.0612191, 28.6884175), keyword="Yemek", language="tr",
+                                 name="Yemek", open_now=True, rank_by="distance", type="restaurant")
+
+    nearby_restaurants = dict()
+
+    for i in gmaps2['results']:
+        name = i['name']
+        geometry = i['geometry']
+        location = geometry['location']
+        lat = location['lat']
+        lng = location['lng']
+        nearby_restaurants[name] = (lat, lng)
+
+    return nearby_restaurants
