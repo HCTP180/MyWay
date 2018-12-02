f063651b151c9ca4cc625879ce3dfcaf4601eb43
ömer <bilalyay@live.com>
2018-12-02 07:35:55 +0300

    requestForAPI.py edited

--- requestForAPI.py
+++ requestForAPI.py
@@ -1,61 +1,43 @@
 import requests
 
-def getFuelLevel(json):
-    return json["fuellevelpercent"]["value"]
+def getFuelLevel():
+    URL = "https://api.mercedes-benz.com/experimental/connectedvehicle/v1/vehicles"
+    fuelURL = URL + "/" + str(carID) + "/" + "fuel"
+    log = requests.get(fuelURL, headers=headers).json()
+    #print(log)
+    return log["fuellevelpercent"]["value"]
 
-def getLocation(json):
+def getLocation():
     liste = []
-    liste.append(json["longitude"]["value"])
-    liste.append(json["latitude"]["value"])
-    liste.append(json["heading"]["value"])
+    URL = "https://api.mercedes-benz.com/experimental/connectedvehicle/v1/vehicles"    
+    locationURL = URL + "/" + str(carID) + "/" + "location"
+    log = requests.get(locationURL, headers=headers).json()
+    #print(log)
+    liste.append(log["longitude"]["value"])
+    liste.append(log["latitude"]["value"])
+    liste.append(log["heading"]["value"])
     return liste
     
-def getTirePressures(json):
+def getTirePressures():
     liste = []
-    liste.append(json["tirepressurerearleft"]["value"])
-    liste.append(json["tirepressurerearright"]["value"])
-    liste.append(json["tirepressurefrontright"]["value"])
-    liste.append(json["tirepressurefrontleft"]["value"])
+    URL = "https://api.mercedes-benz.com/experimental/connectedvehicle/v1/vehicles"
+    tiresURL = URL + "/" + str(carID) + "/" + "tires"
+    log = requests.get(tiresURL, headers=headers).json()
+    #print(log)
+    liste.append(log["tirepressurerearleft"]["value"])
+    liste.append(log["tirepressurerearright"]["value"])
+    liste.append(log["tirepressurefrontright"]["value"])
+    liste.append(log["tirepressurefrontleft"]["value"])
     return liste
 
-def getOdometer(json):
-    return json["odometer"]["value"]
-    
-token = "8c92ef33-65bc-48d9-953e-662dc55495d2"
+def getOdometer():
+    URL = "https://api.mercedes-benz.com/experimental/connectedvehicle/v1/vehicles"
+    odometerURL = URL + "/" + str(carID) + "/" + "odometer"
+    log = requests.get(odometerURL, headers=headers).json()
+    #print(log)
+    return log["odometer"]["value"]
+        
+token = "37bce503-105c-4e0b-8efd-17865cefc7c0"
 URL = "https://api.mercedes-benz.com/experimental/connectedvehicle/v1/vehicles"
-
-
 headers = {"Authorization":"Bearer " + token}
 carID = requests.get(URL, headers=headers).json()[0]["id"]
-
-tiresURL = URL + "/" + str(carID) + "/" + "tires"
-locationURL = URL + "/" + str(carID) + "/" + "location"
-fuelURL = URL + "/" + str(carID) + "/" + "fuel"
-stateofchargeURL = URL + "/" + str(carID) + "/" + "stateofcharge"
-doorsURL = URL + "/" + str(carID) + "/" + "doors"
-odometerURL = URL + "/" + str(carID) + "/" + "odometer"
-
-# print(tiresURL)
-# print(locationURL)
-# print(fuelURL)
-# print(stateofchargeURL)
-# print(doorsURL)
-# print(odometerURL)
-
-log = requests.get(tiresURL, headers=headers).json()
-print(getTirePressures(log))
-
-log = requests.get(locationURL, headers=headers).json()
-print(getLocation(log))
-
-log = requests.get(fuelURL, headers=headers).json()
-print(getFuelLevel(log))
-
-log = requests.get(stateofchargeURL, headers=headers).json()
-print(log)
-
-log = requests.get(doorsURL, headers=headers).json()
-print(log)
-
-log = requests.get(odometerURL, headers=headers).json()
-print(getOdometer(log))
