94a79df04644621290dbf148ff26fc8cc194ad89
emrekosen <emrekosen1@gmail.com>
2018-12-02 10:26:19 +0300

    Error exceptions added.

--- destination.py
+++ destination.py
@@ -1,74 +1,71 @@
-from datetime import datetime
-from datetime import timedelta
 import nearbyPlaces
 import requestForAPI
 import time
 import os
-import responses
-import json
 import googlemaps
-os.system("test&cls")
-Hungry=float(input("How many hours ago did you eat?"))
-Tired=float(input("How many hours do you drive?"))
-origin=input("Where are you?")  
-destination=input("Where do you want to go?")  #Üsküdar,İstanbul
-while(True):
-    #Gas=requestForAPI.getFuelLevel()
-    Gas=5
-    tirepressure=requestForAPI.getTirePressures()
-    kmofcar=requestForAPI.getOdometer();
-    waypoints=[]
-    if(float(Gas)<10 or float(tirepressure[0])<15 or float(tirepressure[1])<15 or float(tirepressure[2])<15 or float(tirepressure[3])<15 ):
-        a=nearbyPlaces.findGasStations()
-        b=list(a.keys())
-        location=str(a[b[0]])[1:-1]
-        waypoints.append(b[0])
-        Gas=100
-    if(kmofcar%20000==0):
-        a=nearbyPlaces.findServices()
-        b=list(a.keys())
-        location=str(a[b[0]])[1:-1]
-        waypoints.append(b[0])
-    if(Hungry>=5):
-        a=nearbyPlaces.findRestaurants()
-        b=list(a.keys())
-        location=str(a[b[0]])[1:-1]
-        waypoints.append(b[0])
-        
-        Hungry=0
-    if(Tired>=4.30):
-        a=nearbyPlaces.findSleepPlaces()
-        b=list(a.keys())
-        location=str(a[b[0]])[1:-1]
-        waypoints.append(b[0])
-        Tired=0
-    
-    gmaps = googlemaps.Client(key="AIzaSyDjSIZfI_fviDx3h-Wo1U9qZsK8fhHrXzA") 
-    routes = gmaps.directions(origin,destination,waypoints=waypoints,optimize_waypoints=True)
+
+try:
     os.system("test&cls")
-    wait="BEST WAY FOR LONG WAY........\n"
-    process="|********************************|\n"
-    for l in wait:
-        print(l,end="",flush=True)
-        time.sleep(0.05)
-    print("Loading...")
-    for l in process:
-        print(l,end="",flush=True)
-        time.sleep(0.07)
-    for i in range(len(waypoints)+1):
-        print(routes[0]["legs"][i]["start_address"])
-        print(routes[0]["legs"][i]["end_address"])
-        if(i!=len(waypoints)):
-            print(waypoints[i])
-        print("Estimated time: "+routes[0]["legs"][i]["duration"]["text"])
-        print("-----------------------------------------------")
-    Hungry+=1
-    Tired+=1
-    time.sleep(3600)
-    
-        
+    Hungry=float(input("How many hours ago did you eat?"))
+    Tired=float(input("How many hours do you drive?"))
+    origin=input("Where are you?")
+    destination=input("Where do you want to go?")  #Üsküdar,İstanbul
+except:
+    print("An error occured in inputs!")
+while(True):
+    try:
+        #Gas=requestForAPI.getFuelLevel()
+        Gas=5
+        tirepressure=requestForAPI.getTirePressures()
+        kmofcar=requestForAPI.getOdometer();
+        waypoints=[]
+        if(float(Gas)<10 or float(tirepressure[0])<15 or float(tirepressure[1])<15 or float(tirepressure[2])<15 or float(tirepressure[3])<15 ):
+            a=nearbyPlaces.findGasStations()
+            b=list(a.keys())
+            location=str(a[b[0]])[1:-1]
+            waypoints.append(b[0])
+            Gas=100
+        if(kmofcar%20000==0):
+            a=nearbyPlaces.findServices()
+            b=list(a.keys())
+            location=str(a[b[0]])[1:-1]
+            waypoints.append(b[0])
+        if(Hungry>=5):
+            a=nearbyPlaces.findRestaurants()
+            b=list(a.keys())
+            location=str(a[b[0]])[1:-1]
+            waypoints.append(b[0])
 
+            Hungry=0
+        if(Tired>=4.30):
+            a=nearbyPlaces.findSleepPlaces()
+            b=list(a.keys())
+            location=str(a[b[0]])[1:-1]
+            waypoints.append(b[0])
+            Tired=0
 
-      
-     
+        gmaps = googlemaps.Client(key="AIzaSyDjSIZfI_fviDx3h-Wo1U9qZsK8fhHrXzA")
+        routes = gmaps.directions(origin,destination,waypoints=waypoints,optimize_waypoints=True)
+        os.system("test&cls")
+        wait="BEST WAY FOR LONG WAY........\n"
+        process="|********************************|\n"
+        for l in wait:
+            print(l,end="",flush=True)
+            time.sleep(0.05)
+        print("Loading...")
+        for l in process:
+            print(l,end="",flush=True)
+            time.sleep(0.07)
+        for i in range(len(waypoints)+1):
+            print(routes[0]["legs"][i]["start_address"])
+            print(routes[0]["legs"][i]["end_address"])
+            if(i!=len(waypoints)):
+                print(waypoints[i])
+            print("Estimated time: "+routes[0]["legs"][i]["duration"]["text"])
+            print("-----------------------------------------------")
+        Hungry+=1
+        Tired+=1
+        time.sleep(3600)
+    except Exception:
+        print("An occured in loop!")
                   
--- nearbyPlaces.py
+++ nearbyPlaces.py
@@ -3,47 +3,56 @@ import googlemaps
 
 def parseJSON(JSON):
     locationDict = dict()
+    try:
+        for i in JSON['results']:
+            name = i['name']
+            geometry = i['geometry']
+            location = geometry['location']
+            lat = location['lat']
+            lng = location['lng']
+            locationDict[name] = (lat, lng)
 
-    for i in JSON['results']:
-        name = i['name']
-        geometry = i['geometry']
-        location = geometry['location']
-        lat = location['lat']
-        lng = location['lng']
-        locationDict[name] = (lat, lng)
-
-    return locationDict
-
+        return locationDict
+    except Exception:
+        print("Can't parse JSON!")
 
 def findGasStations():
-    gmaps = googlemaps.Client(key='AIzaSyDjSIZfI_fviDx3h-Wo1U9qZsK8fhHrXzA')
-    gmaps2 = gmaps.places_nearby(location=(41.0612191, 28.6884175), keyword="Benzin İstasyonu", language="tr",
-                                 name="Benzin İstasyonu", open_now=True, rank_by="distance", type="gas_station")
-    nearby_gas_stations = parseJSON(gmaps2)
+    try:
+        gmaps = googlemaps.Client(key='AIzaSyDjSIZfI_fviDx3h-Wo1U9qZsK8fhHrXzA')
+        gmaps2 = gmaps.places_nearby(location=(41.0612191, 28.6884175), keyword="Benzin İstasyonu", language="tr",
+                                     name="Benzin İstasyonu", open_now=True, rank_by="distance", type="gas_station")
+        nearby_gas_stations = parseJSON(gmaps2)
 
-    return nearby_gas_stations
-
+        return nearby_gas_stations
+    except Exception:
+        print("Can't get gas station data from maps!")
 
 def findRestaurants():
-    gmaps = googlemaps.Client(key='AIzaSyDjSIZfI_fviDx3h-Wo1U9qZsK8fhHrXzA')
-    gmaps2 = gmaps.places_nearby(location=(41.0612191, 28.6884175), keyword="Yemek", language="tr",
-                                 name="Yemek", open_now=True, rank_by="distance", type="restaurant")
-    nearby_restaurants = parseJSON(gmaps2)
-    return nearby_restaurants
-
+    try:
+        gmaps = googlemaps.Client(key='AIzaSyDjSIZfI_fviDx3h-Wo1U9qZsK8fhHrXzA')
+        gmaps2 = gmaps.places_nearby(location=(41.0612191, 28.6884175), keyword="Yemek", language="tr",
+                                     name="Yemek", open_now=True, rank_by="distance", type="restaurant")
+        nearby_restaurants = parseJSON(gmaps2)
+        return nearby_restaurants
+    except Exception:
+        print("Can't get restaurant data from maps!")
 
 def findServices():
-    gmaps = googlemaps.Client(key='AIzaSyDjSIZfI_fviDx3h-Wo1U9qZsK8fhHrXzA')
-    gmaps2 = gmaps.places_nearby(location=(41.0612191, 28.6884175), keyword="Tamir", language="tr",
-                                 name="Tamir", open_now=True, rank_by="distance", type="car_repair")
-    nearby_services = parseJSON(gmaps2)
-    return nearby_services
-
+    try:
+        gmaps = googlemaps.Client(key='AIzaSyDjSIZfI_fviDx3h-Wo1U9qZsK8fhHrXzA')
+        gmaps2 = gmaps.places_nearby(location=(41.0612191, 28.6884175), keyword="Tamir", language="tr",
+                                     name="Tamir", open_now=True, rank_by="distance", type="car_repair")
+        nearby_services = parseJSON(gmaps2)
+        return nearby_services
+    except Exception:
+        print("Can't get service data from maps!")
 
 def findSleepPlaces():
-    gmaps = googlemaps.Client(key='AIzaSyDjSIZfI_fviDx3h-Wo1U9qZsK8fhHrXzA')
-    gmaps2 = gmaps.places_nearby(location=(41.0612191, 28.6884175), keyword=['Hotel', 'Otel', 'Pansiyon', 'Hostel'], language="tr",
-                                 name="Dinlenme", open_now=True, rank_by="distance", type="lodging")
-    nearby_sleep_places = parseJSON(gmaps2)
-    return nearby_sleep_places
-
+    try:
+        gmaps = googlemaps.Client(key='AIzaSyDjSIZfI_fviDx3h-Wo1U9qZsK8fhHrXzA')
+        gmaps2 = gmaps.places_nearby(location=(41.0612191, 28.6884175), keyword=['Hotel', 'Otel', 'Pansiyon', 'Hostel'], language="tr",
+                                     name="Dinlenme", open_now=True, rank_by="distance", type="lodging")
+        nearby_sleep_places = parseJSON(gmaps2)
+        return nearby_sleep_places
+    except Exception:
+        print("Can't get place data from maps!")
